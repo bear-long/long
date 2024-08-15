91d45940ab3ed5f1451611386b25b0abfd3b9513
bear-long <64183198+bear-long@users.noreply.github.com>
2024-08-15 15:11:25 +0800

    Add files via upload

---
+++ demo.cpp
@@ -0,0 +1,20 @@
+#include <opencv2/opencv.hpp>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <Multithreading.h>
+#include <HK_camera.h>
+#include <Multithreading.h>
+template <typename T>
+
+int main() {
+    // 定义每个队列的大小
+    const size_t queueSize = 10;
+    vector<MyCircularQueue> queue1(queueSize);
+    vector<MyCircularQueue> queue2(queueSize);
+    vector<MyCircularQueue> queue3(queueSize);
+
+}
+
+    return 0;
+}
